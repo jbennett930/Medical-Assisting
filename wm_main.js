57b23715686b7593c6e43589955569021a82c487
Brenden Riley <briley001@west-mec.org>
2019-04-25 15:26:52 -0700

    Pictures
    Small CSS changes, added a lot of pictures, which should change but don't yet

--- wm_main.js
+++ wm_main.js
@@ -2,3 +2,19 @@
         Authors: Brenden Riley, Dylan Kelley
         Date: 4.3.19
 */
+
+window.addEventListener("load", setup);
+
+function setup() {
+        setInterval("swapPics()", 1000);
+}
+
+function randomNumber(size) {
+        return Math.ceil(Math.random() * size)
+}
+
+function swapPics() {
+        document.getElementById("aboutImg").setAttribute("src", "pictures/Medphoto" + randomNumber(13) + ".jpg")
+        document.getElementById("ctsoImg").setAttribute("src", "pictures/Medphoto" + randomNumber(13) + ".jpg")
+        document.getElementById("contactImg").setAttribute("src", "pictures/Medphoto" + randomNumber(13) + ".jpg")
+}
