18ef90698fd8192f195d1d321a540cf14a8ddda4
Brenden Riley <briley001@west-mec.org>
2019-04-30 14:07:17 -0700

    Buttons
    Added buttons that change the contact information that appears, depending on the button pressed. Changed the pages font, to nunito-sans, which is one of the WM brand standards.

--- wm_main.js
+++ wm_main.js
@@ -8,6 +8,9 @@ window.addEventListener("load", setup);
 function setup() {
         swapPics();
         setInterval("swapPics()", 10000);
+        document.getElementById("NWbutton").addEventListener("click", changeContactInfo)
+        document.getElementById("NEbutton").addEventListener("click", changeContactInfo)
+        document.getElementById("SWbutton").addEventListener("click", changeContactInfo)
 }
 
 function randomNumber(size) {
@@ -19,4 +22,41 @@ function swapPics() {
         document.getElementById("aboutImg").setAttribute("src", "pictures/Medphoto" + (randNum) + ".jpg")
         document.getElementById("ctsoImg").setAttribute("src", "pictures/Medphoto" + (randNum + 1) + ".jpg")
         document.getElementById("contactImg").setAttribute("src", "pictures/Medphoto" + (randNum + 2) + ".jpg")
+}
+
+function changeContactInfo() {
+        var contactInfo = document.getElementById("contactArticle")
+        contactInfo.innerHTML = "";
+        if (event.target.value == 1) {
+                var newContactElement1 = document.createElement("p");
+                var newContactElement2 = document.createElement("p");
+                var newPhone = document.createElement("p");
+                newContactElement1.appendChild(document.createTextNode("13201 W. Grand Avenue"));
+                newContactElement2.appendChild(document.createTextNode("Surprise, AZ 85374"));
+                newPhone.appendChild(document.createTextNode("623.877.6914"));
+                contactInfo.appendChild(newContactElement1);
+                contactInfo.appendChild(newContactElement2);
+                contactInfo.appendChild(newPhone);
+        } else if (event.target.value == 2) {
+                var newContactElement1 = document.createElement("p");
+                var newContactElement2 = document.createElement("p");
+                var newPhone = document.createElement("p");
+                newContactElement1.appendChild(document.createTextNode("1617 W Williams Dr."));
+                newContactElement2.appendChild(document.createTextNode("Phoenix, AZ 85027"));
+                newPhone.appendChild(document.createTextNode("623.435.4920"));
+                contactInfo.appendChild(newContactElement1);
+                contactInfo.appendChild(newContactElement2);
+                contactInfo.appendChild(newPhone);
+        } else if (event.target.value == 3) {
+                var newContactElement1 = document.createElement("p");
+                var newContactElement2 = document.createElement("p");
+                var newPhone = document.createElement("p");
+                newContactElement1.appendChild(document.createTextNode("500 N. Verrado Way"));
+                newContactElement2.appendChild(document.createTextNode("Buckeye, AZ 85326"));
+                newPhone.appendChild(document.createTextNode("623.872.6260"));
+                contactInfo.appendChild(newContactElement1);
+                contactInfo.appendChild(newContactElement2);
+                contactInfo.appendChild(newPhone);
+        }
+
 }
